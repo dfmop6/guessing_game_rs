30cb75cdf0dd14f543b426b6777361510e494492
issamevead <ss.elmachehour@gmail.com>
2022-12-03 14:14:29 +0100

    first commit

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+// game based on how many time you guess untill y get the correct value
+// once u right your guess the robot will guide u if corret or less/greater than the win.
+
+fn main() {
+    println!("Hello, world!");
+}
