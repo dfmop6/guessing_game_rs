1b0ef0ca71bc12685e903377a52b9b3b07c8df80
issamevead <ss.elmachehour@gmail.com>
2022-12-05 21:21:17 +0100

    add basic functionnalities to the code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,33 @@
 // game based on how many time you guess untill y get the correct value
 // once u right your guess the robot will guide u if corret or less/greater than the win.
-use std::io;
+
+use std::{cmp::Ordering, io};
+
+use rand::Rng;
 
 fn main() {
     println!("Welcome to the guessing game");
     println!("please enter your guess :");
 
-    let mut guess = String::new();
+    let secret_guess = rand::thread_rng().gen_range(1, 101);
 
-    io::stdin().read_line(&mut guess).expect("failed to read");
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read, please try again");
 
-    println!("your guess: {}", guess);
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("failed to parse, please try again");
+
+        println!("your guess: {}", guess);
+
+        match guess.cmp(&secret_guess) {
+            Ordering::Equal => println!("Great, you win!"),
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
